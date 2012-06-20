8bb6ac95b216f38164489008d7520d917f3a0d9c
gbvbahia01 <gbvbahia01@gmail.com>
2012-06-20 00:16:11 +0000

    MakeIn

--- src/main/java/br/com/gbvbahia/maker/works/MakeIn.java
+++ src/main/java/br/com/gbvbahia/maker/works/MakeIn.java
@@ -4,9 +4,19 @@
  */
 package br.com.gbvbahia.maker.works;
 
+import br.com.gbvbahia.i18n.I18N;
+import br.com.gbvbahia.maker.factories.types.NumberFactory;
+import br.com.gbvbahia.maker.log.LogInfo;
+import br.com.gbvbahia.maker.types.wrappers.MakeInteger;
+import br.com.gbvbahia.maker.types.wrappers.common.MakeNumber;
 import br.com.gbvbahia.maker.works.common.ValuePropertiesFactory;
+import br.com.gbvbahia.maker.works.execeptions.MakeWorkException;
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
@@ -15,6 +25,10 @@ import java.util.regex.Pattern;
  */
 public class MakeIn implements ValuePropertiesFactory {
 
+    /**
+     * Guarda informações que serão necessárias para popular o field.
+     */
+    private List<String> inList;
     /**
      * No arquivo make.properties deve estár definido no valor para o
      * field: "in\\{.*\\}(\\[.+\\])?".<br> Regex: deve iniciar com
@@ -36,18 +50,65 @@ public class MakeIn implements ValuePropertiesFactory {
 
     @Override
     public boolean workValue(final String value) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        LogInfo.logDebugInformation("MakeIn",
+                I18N.getMsg("workValueMake", value));
+        Matcher matcher = PATTERN.matcher(value);
+        if (matcher.find()) {
+            LogInfo.logDebugInformation("MakeIn",
+                    I18N.getMsg("isWork", "In", value));
+            LogInfo.logDebugInformation("MakeIn", matcher.group());
+            popularInfo(value);
+            return true;
+        } else {
+            LogInfo.logDebugInformation("MakeIn",
+                    I18N.getMsg("notIsWork", "In", value));
+            return false;
+        }
     }
 
     @Override
     public <T> boolean isWorkWith(final Field f, final T entity) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        for (MakeNumber number : NumberFactory.NUMBERS_FACTORYS) {
+            if (number.isMyType(f)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public <T> void makeValue(final String testName, final Field f,
             final T entity, final boolean makeRelationships)
             throws IllegalAccessException, IllegalArgumentException {
-        throw new UnsupportedOperationException("Not supported yet.");
+          for (MakeNumber number : NumberFactory.NUMBERS_FACTORYS) {
+            if (number.isMyType(f)) {
+                try {
+                    number.insertValue(f, entity, inList.get(MakeInteger.getIntervalo(0, inList.size() -1)));
+                } catch (NumberFormatException nf) {
+                    nf.printStackTrace();
+                    throw new MakeWorkException("MakeIn", "NumberFormatException",
+                            new String[]{inList.toString()}, nf);
+                }
+            }
+        }
+    }
+
+    /**
+     * Popula info (CollectionsHelper) com informações necessárias
+     * para criar e popular o List.
+     *
+     * @param value Valor declarado no make.properties.
+     * @throws MakeWorkException Se não encontrar a classe informada
+     * no properties ou conversão numérica não for possível.
+     */
+    private void popularInfo(final String value) {
+        String in = StringUtils.substringBetween(value, "{", "}");
+        String split = StringUtils.substringBetween(value, "[", "]");
+        if (StringUtils.isBlank(split)) {
+            split = ",";
+        }
+        inList = Arrays.asList(StringUtils.split(in, split));
+        LogInfo.logDebugInformation("MakeIn", "split:" + split
+                + " in: " + in);
     }
 }
